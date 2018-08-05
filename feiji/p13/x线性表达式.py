f58f13de14ab6331df861fba7a7ee13cc2e6fccd
dzb <1083027306@qq.com>
2018-08-05 20:29:46 +0800

    18/8/5

--- feiji/p13/x线性表达式.py
+++
@@ -1,9 +0,0 @@
-def wai(a,b):
-    def nei(x):
-        print('y = %d*%d+%d'% (a,x,b))
-        y = a*x+b
-        return 'y的值是:%d'% y
-    return nei
-a = wai(1,2)
-print(a(3))
-
--- feiji/test/1test.py
+++
@@ -1,9 +0,0 @@
-def w1(fun):
-    def inner():
-        print('正在验证')
-        fun()
-    return inner
-@w1
-def Test():
-    print('哈哈')
-Test()
